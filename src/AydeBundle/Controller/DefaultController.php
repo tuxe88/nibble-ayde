651e1693f345d8d5c8c43cde2111d62f98ebcd9c
Tuxe88 <v.tourrilhes@gmail.com>
2016-06-17 14:20:26 -0300

    nada, boludeces

--- src/AydeBundle/Controller/DefaultController.php
+++ src/AydeBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace AydeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AydeBundle\Utils\CostReportGenerator;
+use AydeBundle\Entity\Imputation;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -16,10 +19,35 @@ class DefaultController extends Controller
     }
     
     /**
-     * @Route("/carga")
+     * @Route("/carga", name="_carga")
      */
-    public function cargaAction()
+    public function cargaAction(Request $request)
     {
+        //TODO
+        if ($request->getMethod() == 'POST') {
+            
+            $fecha = $request->query->get('datepicker-dia-carga');
+            $user = $request->query->get('select-user');
+            $projects = $this->getDoctrine()->getManager()->getRepository('AydeBundle:Project')->findAll();
+            $em = $this->getDoctrine()->getManager();
+            
+            foreach($projects as $actualProject){
+                
+                $imputation = new Imputation();
+                $imputation->setDate($date);
+                $imputation->setProject();
+                $imputation->setUser();
+                $imputation->setDedication();
+                
+                //busco los datos, creo la entidad y la persisto
+                $em->persist($imputation);
+                //$em->flush();
+            }
+            
+            
+            return $this->render('AydeBundle:Default:carga_exitosa.html.twig');
+        
+        }
         
         $usuarios= $this->getDoctrine()->getManager()->getRepository('AydeBundle:User')->findAll();
         
@@ -27,15 +55,37 @@ class DefaultController extends Controller
         
         
         return $this->render('AydeBundle:Default:carga_proyectos.html.twig',array("proyectos"=>$proyectos,
-                                                                                    "usuarios"=>$usuarios));
+                                                                                    "usuarios"=>$usuarios));                        
     }
     
+    
     /**
-     * @Route("/resumen")
+     * @Route("/consulta_resumen", name="_consulta_resumen" )
      */
-    public function resumenAction()
+    public function consultaResumenAction(Request $request)
     {
-        return $this->render('AydeBundle:Default:resumen.html.twig');
+        if ($request->getMethod() == 'POST') {
+            
+            $fechaInicial = $request->query->get('datepicker-inicio');
+            $fechaFinal = $request->query->get('datepicker-fin');
+            $projects = $this->getDoctrine()->getManager()->getRepository('AydeBundle:Imputation')->findAll();
+            
+            //$crg = new CostReportGenerator();
+            //$crg->generateCostReport($projects, $fechaInicial, $fechaFinal);
+        
+            foreach($projects as $actualProject){
+              
+                $imputations = $this->getDoctrine()->getManager()->getRepository('AydeBundle:Imputation')->findBy(array("project"=>$actualProject,
+                                                                                                                        ));
+                
+                
+            }
+            
+            return $this->render('AydeBundle:Default:resumen.html.twig');
+        
+        }
+        
+        return $this->render('AydeBundle:Default:consulta_resumen.html.twig');
     }
     
     /**
